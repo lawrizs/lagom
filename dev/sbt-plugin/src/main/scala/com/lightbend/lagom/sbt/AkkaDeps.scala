f322279a5b8b83273b91d3434a52c714ccaed023
Laurynas Siksnys <laurynas@flexshape.dk>
2023-08-24 12:44:47 +0200

    feat(dev/sbt-plugin): Added an object with Akka dependencies in Lagom-specific version, to be able to use in Lagom projects

--- dev/sbt-plugin/src/main/scala/com/lightbend/lagom/sbt/AkkaDeps.scala
+++ dev/sbt-plugin/src/main/scala/com/lightbend/lagom/sbt/AkkaDeps.scala
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+
 package com.lightbend.lagom.sbt
 
 import sbt._
@@ -7,10 +11,9 @@ import com.lightbend.lagom.core.LagomVersion
 object AkkaDeps {
 
   // Here, we can override Akka, Play, Akka gRPC versions
-  val playVersion = LagomVersion.play
-  val akkaVersion = LagomVersion.akka // Works with 2.7.0, but taking a lower one, since Play 2.8.18 uses 2.8.18
+  val playVersion     = LagomVersion.play
+  val akkaVersion     = LagomVersion.akka // Works with 2.7.0, but taking a lower one, since Play 2.8.18 uses 2.8.18
   val akkaHttpVersion = LagomVersion.akkaHttp // Works with 10.4.0, but selected 10.2.10 to match the Akka version
-
 
   /** All relevant Akka, Akka HTTP, Play, Testkit modules */
   def lagomAkkaDeps: Seq[sbt.ModuleID] =
@@ -18,36 +21,36 @@ object AkkaDeps {
 
   /** Akka module Ids we use */
   def modulesIdsAkka: scala.Seq[ModuleID] = Seq(
-    "com.typesafe.akka" %% "akka-discovery" % akkaVersion,
-    "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
+    "com.typesafe.akka" %% "akka-discovery"              % akkaVersion,
+    "com.typesafe.akka" %% "akka-cluster-sharding"       % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster-sharding-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
-    "com.typesafe.akka" %% "akka-cluster-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-coordination" % akkaVersion,
-    "com.typesafe.akka" %% "akka-distributed-data" % akkaVersion,
-    "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
-    "com.typesafe.akka" %% "akka-persistence-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-persistence-query" % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-stream-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-serialization-jackson" % akkaVersion
+    "com.typesafe.akka" %% "akka-cluster-tools"          % akkaVersion,
+    "com.typesafe.akka" %% "akka-cluster-typed"          % akkaVersion,
+    "com.typesafe.akka" %% "akka-coordination"           % akkaVersion,
+    "com.typesafe.akka" %% "akka-distributed-data"       % akkaVersion,
+    "com.typesafe.akka" %% "akka-persistence"            % akkaVersion,
+    "com.typesafe.akka" %% "akka-persistence-typed"      % akkaVersion,
+    "com.typesafe.akka" %% "akka-persistence-query"      % akkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf"               % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j"                  % akkaVersion,
+    "com.typesafe.akka" %% "akka-stream"                 % akkaVersion,
+    "com.typesafe.akka" %% "akka-stream-typed"           % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor-typed"            % akkaVersion,
+    "com.typesafe.akka" %% "akka-serialization-jackson"  % akkaVersion
   )
 
   /** Akka http module Ids we use */
   def modulesIdsAkkaHttp: scala.Seq[ModuleID] = Seq(
-    "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
-    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
+    "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion,
+    "com.typesafe.akka" %% "akka-http-core"       % akkaHttpVersion,
     "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
-    "com.typesafe.akka" %% "akka-http2-support" % akkaHttpVersion
+    "com.typesafe.akka" %% "akka-http2-support"   % akkaHttpVersion
   )
 
   def modulesIdsAkkaTestkit: scala.Seq[ModuleID] = Seq(
-    "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+    "com.typesafe.akka" %% "akka-testkit"             % akkaVersion,
     "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion,
-    "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion
+    "com.typesafe.akka" %% "akka-stream-testkit"      % akkaVersion
   )
 
   /** Play module Ids we use */
