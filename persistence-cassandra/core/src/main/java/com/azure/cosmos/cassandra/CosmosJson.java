bad190e8cf162594ec895fd316f6b23eecd4be75
Laurynas Siksnys <laurynas@flexshape.dk>
2023-11-10 10:07:53 +0100

    fix(persistence-cassandra): Fixed file headers

--- persistence-cassandra/core/src/main/java/com/azure/cosmos/cassandra/CosmosJson.java
+++ persistence-cassandra/core/src/main/java/com/azure/cosmos/cassandra/CosmosJson.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+
+// Imported from: https://github.com/David-Noble-at-work/azure-cosmos-cassandra-extensions/tree/release/java-driver-4/1.1.2/
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
